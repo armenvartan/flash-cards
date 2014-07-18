d0714d8a6ddb5fef9b993686ba791acf3154d24a
Armen Vartan & Fahia Mohamed <pair+armenvartan.fahiamoh@devbootcamp.com>
2014-07-18 16:49:42 -0400

    added flashcard edit

--- app/controllers/flashcards.rb
+++ app/controllers/flashcards.rb
@@ -9,3 +9,19 @@ post '/decks/:did/flashcards/create' do
   @deck.flashcards.create(params[:flashcard])
   redirect "decks/#{@deck.id}"
 end
+
+get '/decks/:did/flashcards/:fid' do
+  @flashcard = Flashcard.find(params[:fid])
+  erb :'flashcards/edit'
+end
+
+put '/decks/:did/flashcards/:fid' do
+  deck = Deck.find(params[:did])
+  flashcard = deck.flashcards.find(params[:fid])
+  flashcard.update(params[:flashcard])
+  redirect "/decks/#{deck.id}"
+end
+
+delete '/decks/:did/flashcards/:fid' do
+
+end
