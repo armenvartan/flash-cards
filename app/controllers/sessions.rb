be70afc4089ee0be02b15a278d1456aba4b38d09
Fahia Mohamed  & Stephen Estrada <pair+fahiamoh.stcrestrada@devbootcamp.com>
2014-07-18 16:50:22 -0400

    completed sign in page and login page routes

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -1,4 +1,42 @@
+enable :sessions
+
+get '/' do
+  @user ||= session[:email][:password]
+  erb :index
+end
+
+post '/' do
+
+end
+
+get '/login' do
+
+  erb :'/sessions/_login'
+end
+
+post '/login' do
+  @user = User.authenticate(params[:user][:email], params[:user][:password])
+  if session[:email][:password] == @user
+    redirect '/'
+  else
+    redirect '/login'
+  end
+end
+
+get '/signup' do
+
+  erb :'/sessions/_sign_up'
+end
+
+post '/signup' do
+  @user = User.create(params[:user)
+  redirect('/')
+end
+
+
+
 post '/logout' do
 
   redirect '/'
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   has_many :decks, through: :collaborators
+
+  def self.authenticate(email, password)
+    user = User.find_by email: email
+    return user if user.password == password
+    return nil
+  end
 end
