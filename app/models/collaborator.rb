49ef4dfb4c8a749fb73c6b0fab398b3d44e738c7
Armen Vartan & Fahia Mohamed <pair+armenvartan.fahiamoh@devbootcamp.com>
2014-07-18 11:59:36 -0400

    added associations to models

--- app/models/collaborator.rb
+++ app/models/collaborator.rb
@@ -1,3 +1,4 @@
 class Collaborator < ActiveRecord::Base
-
+  has_many :users
+  has_many :decks
 end
--- app/models/flashcard.rb
+++ app/models/flashcard.rb
@@ -1,3 +1,3 @@
 class Flashcard < ActiveRecord::Base
-  # Remember to create a migration!
+  belongs_to :deck
 end
