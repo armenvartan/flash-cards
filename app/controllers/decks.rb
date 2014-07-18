21959573334d34f39849590d6a959be1fcff8102
Armen Vartan & Fahia Mohamed <pair+armenvartan.fahiamoh@devbootcamp.com>
2014-07-18 16:24:35 -0400

    flushed out decks controller

--- app/controllers/decks.rb
+++ app/controllers/decks.rb
@@ -1,20 +1,30 @@
 get '/' do
-
+  @decks = Deck.all
   erb :'decks/index'
 end
 
 get '/decks/create' do
-
   erb :'decks/create'
 end
 
-get '/decks/:did' do
+post '/decks/create' do
+  deck = Deck.new(params[:deck])
+  if deck.valid?
+    deck.save
+    redirect '/'
+  else
+    @errors = deck.errors
+    erb :'decks/create'
+  end
+end
 
+get '/decks/:did' do
+  @deck = Deck.find(params[:did])
   erb :'decks/show'
 end
 
 get '/decks/:did/edit' do
-
+  @deck = Deck.find(params[:did])
   erb :'decks/edit'
 end
 
